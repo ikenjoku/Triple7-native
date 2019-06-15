e05a6bfa9ae9bbc8bb81c02aa300dd663e7998f1
Ike Njoku <ikeenjoku@gmail.com>
2019-06-15 20:49:20 +0100

    configure initial state

---
+++ src/redux/reducers/initialState.js
@@ -0,0 +1,14 @@
+export default {
+  appReducer: {
+    isLoading: false,
+  },
+  authReducer: {
+    user: null,
+    error: null,
+  },
+  mealReducer: {
+    isLoading: false,
+    menu: [],
+    error: null,
+  }
+}
