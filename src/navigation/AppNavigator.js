1d2950ae4e174f64e9d87c1080c1930e3a84bca2
Ike Njoku <ikeenjoku@gmail.com>
2019-06-22 20:03:55 +0100

    use custom navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-
+import { connect } from 'react-redux';
+import { Root } from 'native-base';
 import MainNavigator from './MainNavigator';
 import AuthNavigator from './AuthNavigator';
 import OnboardingNavigator from './OnboardingNavigator';
+import { getData } from "../utils/asyncStore";
+import NavigationService from "./NavigationService";
 
 const TopLevelNavigator = createSwitchNavigator({
   Onboard:OnboardingNavigator,
@@ -11,4 +14,50 @@ const TopLevelNavigator = createSwitchNavigator({
   Auth: AuthNavigator,
 });
 
-export default createAppContainer(TopLevelNavigator);
+const TopLevelAppNavigator = createAppContainer(TopLevelNavigator);
+
+class RootApp extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    this.fetchToken();
+  }
+
+  fetchToken = () => {
+    const { user } = this.props;
+    getData('@triple-tutorial-cookie')
+      .then(tutorialToken => {
+        if (!tutorialToken) {
+          NavigationService.navigate('Onboard');
+        } else {
+          if (user) {
+            NavigationService.navigate('Main');
+          } else {
+            NavigationService.navigate('Auth');
+          }
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  };
+
+  render() {
+    return (
+      <Root>
+        <TopLevelAppNavigator
+          ref={navigatorRef => {
+            NavigationService.setTopLevelNavigator(navigatorRef);
+          }}
+        />
+      </Root>
+    );
+  }
+}
+const mapStateToProps = ({ authReducer }) => ({
+  user: authReducer.user,
+});
+
+export default connect(mapStateToProps, { })(RootApp);
