dac5bb47802cea7e025be1b22b0791fd9c888b58
Ike Njoku <ikeenjoku@gmail.com>
2019-06-23 19:19:26 +0100

    add cart action types

--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -12,3 +12,8 @@ export const SIGNUP_USER_SUCCESS = 'SIGNUP_USER_SUCCESS';
 export const SIGNUP_USER_FAILURE = 'SIGNUP_USER_FAILURE';
 
 export const LOGOUT_USER = 'LOGOUT_USER';
+
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const CLEAR_CART = 'CLEAR_CART';
+export const HOME_DELIVERY = 'HOME_DELIVERY';
