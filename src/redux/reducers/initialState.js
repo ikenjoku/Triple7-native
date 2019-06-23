dc6e9af20c0f714521bd4ddb5832b6bcb19a7e02
Ike Njoku <ikeenjoku@gmail.com>
2019-06-23 19:20:36 +0100

    update initial state with cart

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -10,5 +10,25 @@ export default {
     isLoading: false,
     menu: [],
     error: null,
+  },
+  cartReducer: {
+    homeDelivery: false,
+    cart: [
+      {
+        name: 'Rice and Chicken',
+        qty: 3,
+        price: 600,
+      },
+      {
+        name: 'Semo and Efo',
+        qty: 3,
+        price: 2100,
+      },
+      {
+        name: 'Ingera',
+        qty: 3,
+        price: 900,
+      },
+    ],
   }
 }
