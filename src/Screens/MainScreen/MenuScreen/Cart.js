d90d3d63917415d18bb63d19279efb0fc8ab107f
Ike Njoku <ikeenjoku@gmail.com>
2019-06-23 15:12:57 +0100

    create empty cart view

--- src/Screens/MainScreen/MenuScreen/Cart.js
+++ src/Screens/MainScreen/MenuScreen/Cart.js
@@ -1,20 +1,59 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import LottieView from 'lottie-react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button, Icon } from "react-native-elements";
+import CustomHeader from '../../../components/Header';
 
+
+class AnimatedCartIcon extends Component {
+  render() {
+    return <LottieView source={require('../../../assets/cart1.json')} autoPlay loop />;
+  }
+}
 class Cart extends Component {
 
   static navigationOptions = {
+    header: null,
+  }
+
+  renderRightHeaderIcon = (navigation) => {
+    return <Icon
+      name='home'
+      size={35}
+      color='#fff'
+      underlayColor='transparent'
+      onPress={() => navigation.navigate('MenuList')}
+    />
   }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <View style={[{ backgroundColor: '#60992D' }, styles.container]}>
-        <Text>CART-SCREEN</Text>
-        <Button
-          title="Make Payment"
-          onPress={() => navigate('Payment')}
-        />
+      <View style={[{ backgroundColor: '#eaeaea' }, styles.container]}>
+      <CustomHeader
+        title={'Cart'}
+        navigation={this.props.navigation}
+        rightComponent={this.renderRightHeaderIcon}
+      />
+      <View style={{height: '40%'}}>
+      <AnimatedCartIcon />
+      </View>
+      <View style={{paddingLeft: '3%', paddingRight: '3%', paddingBottom: '3%', flex:1}}>
+        <Text style={{textAlign:'center', fontSize: 25, fontWeight:'500'}}>Your cart is empty</Text>
+        <Text style={{textAlign:'center', fontSize: 15, fontWeight:'400'}}>Load up your basket with some yummy meals</Text>
+          <Button
+            raised
+            title="See Today's Menu"
+            onPress={() => navigate('MenuList')}
+            buttonStyle={{
+              backgroundColor:'#B32F20'
+            }}
+            containerStyle={{
+              marginTop:'auto'
+            }}
+          />
+      </View>
+        
       </View>
     );
   }
@@ -25,8 +64,8 @@ export default Cart;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
+    // flexDirection: 'column',
+    // justifyContent: 'center',
+    // alignItems: 'center',
   },
 });
