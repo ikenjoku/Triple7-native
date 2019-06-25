9e26491b0dee5d7b641069666b92f81c1c73ce3b
Ike Njoku <ikeenjoku@gmail.com>
2019-06-25 20:02:15 +0100

    clear cart initial state

--- src/redux/reducers/initialState.js
+++ src/redux/reducers/initialState.js
@@ -13,22 +13,6 @@ export default {
   },
   cartReducer: {
     homeDelivery: false,
-    cart: [
-      {
-        name: 'Rice and Chicken',
-        qty: 3,
-        price: 600,
-      },
-      {
-        name: 'Semo and Efo',
-        qty: 3,
-        price: 2100,
-      },
-      {
-        name: 'Ingera',
-        qty: 3,
-        price: 900,
-      },
-    ],
+    cart: [],
   }
 }
