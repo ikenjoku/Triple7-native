4617fc8c24b7c3beb208fec0bae89b091cbc70f5
Ike Njoku <ikeenjoku@gmail.com>
2019-06-25 20:01:27 +0100

    reuse header component

--- src/Screens/MainScreen/MenuScreen/MenuList.js
+++ src/Screens/MainScreen/MenuScreen/MenuList.js
@@ -7,7 +7,8 @@ import { Card, Icon, Button, Badge, Header, withBadge } from 'react-native-eleme
 import { fetchMenu } from "../../../redux/actions/mealActions";
 import { addToCart } from "../../../redux/actions/cartActions";
 import AnimatedLoader from "../../../components/animatedLoader";
-
+import AnimatedCartIcon from '../../../components/animatedCartIcon';
+import CustomHeader from "../../../components/Header";
 class MenuList extends Component {
 
   static navigationOptions = {
@@ -29,19 +30,11 @@ class MenuList extends Component {
   }
 
   renderRightHeaderIcon = (navigation) => {
-    const BadgedIcon = withBadge(0)(Icon)
-    return <BadgedIcon
-      type="antdesign"
-      name="shoppingcart"
-      color='#fff'
-      size={35}
-      underlayColor='transparent'
-      onPress={() => navigation.navigate('Cart')}
-      />
+    return <AnimatedCartIcon navigation={navigation} />
   }
 
   renderDish = (meal) => {
-    const { navigation, cart, addToCart } =  this.props;
+    const { navigation, cart, addToCart } = this.props;
     if (meal.category !== 'Drinks') {
       return (
         <Animatable.View key={meal._id} animation="fadeInRightBig" duration={400}>
@@ -89,51 +82,48 @@ class MenuList extends Component {
               marginTop: '3%'
             }}
           >
-          <View style={{ flexDirection: 'row', justifyContent: 'space-around'  }}>
-            <View>
-            <Image
-              source={{ uri: meal.imgurl }}
-              style={{ width: 100, height: 100 }}
-              PlaceholderContent={<ActivityIndicator />}
-            />
+            <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+              <View>
+                <Image
+                  source={{ uri: meal.imgurl }}
+                  style={{ width: 100, height: 100 }}
+                  PlaceholderContent={<ActivityIndicator />}
+                />
+              </View>
+              <View style={{ flexDirection: 'column' }}>
+                <Badge value={meal.category} textStyle={{ color: '#2FBE74' }} badgeStyle={{ backgroundColor: '#fff', padding: 5, borderColor: '#2FBE74' }} />
+                <Text style={{ marginBottom: 10, fontWeight: '700' }}>
+                  {meal.name}
+                </Text>
+                <Text style={{ marginBottom: 10 }}>
+                  {meal.description}
+                </Text>
+                <Badge activeOpacity={0.8} onPress={() => addToCart(cart, { name: meal.name, price: meal.price })} value="+ Add to Cart" badgeStyle={{ backgroundColor: '#B32F20', padding: 15 }} />
+              </View>
             </View>
-            <View style={{ flexDirection: 'column' }}>
-            <Badge value={meal.category} textStyle={{ color: '#2FBE74' }} badgeStyle={{ backgroundColor: '#fff', padding: 5, borderColor: '#2FBE74' }} />
-              <Text style={{ marginBottom: 10, fontWeight: '700' }}>
-                {meal.name}
-              </Text>
-              <Text style={{ marginBottom: 10 }}>
-              {meal.description}
-            </Text>
-              <Badge activeOpacity={0.8} onPress={() => addToCart(cart, { name: meal.name, price: meal.price })} value="+ Add to Cart" badgeStyle={{ backgroundColor: '#B32F20', padding: 15 }} />
-            </View>
-          </View>
           </Card>
         </Animatable.View>
-    );
+      );
     }
   }
 
   render() {
     const { menu, navigation, isLoading } = this.props;
-    const { navigate } = navigation;
     return (
       <View style={styles.container}>
-        <Header
-          statusBarProps={{ barStyle: 'light-content', backgroundColor: '#24a060' }}
-          containerStyle={styles.header}
-          leftComponent={this.renderMenuIcon(navigation)}
-          centerComponent={{ text: 'Menu', style: styles.titleStyle }}
-          rightComponent={this.renderRightHeaderIcon(navigation)}
+        <CustomHeader
+          title={'Menu'}
+          navigation={navigation}
+          rightComponent={this.renderRightHeaderIcon}
         />
         {isLoading ? <AnimatedLoader loading={isLoading} />
-        : (
-        <ScrollView style={[{ flex: 1, paddingBottom: '30%' }, styles.container]}>
-          {
-            menu && menu.map(this.renderDish)
-          }
-        </ScrollView>
-        )}
+          : (
+            <ScrollView style={[{ flex: 1, paddingBottom: '30%' }, styles.container]}>
+              {
+                menu && menu.map(this.renderDish)
+              }
+            </ScrollView>
+          )}
       </View>
     );
   }
