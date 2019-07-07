f7d1f0a9038369e74fe59d5c73e32e9a36dc5392
Ike Njoku <ikeenjoku@gmail.com>
2019-07-07 09:43:16 +0100

    update app navigation

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -21,87 +21,32 @@ const MainStack = createDrawerNavigator({
           name='food-fork-drink'
           type='material-community'
           size={24}
-          color={ navigation.isFocused ? '#006025' : '#777f7c' }
+          color='#777f7c'
         />
       ),
     }),
   },
   Reservation: {
     screen: Reservation,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Reserve Table',
-      drawerIcon: () => (
-        <Icon
-          name='tags'
-          type='antdesign'
-          size={24}
-          color={ navigation.isFocused ? '#006025' : '#777f7c' }
-        />
-      ),
-    })
   },
   Orders: {
     screen: Orders,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Order History',
-      drawerIcon: () => (
-        <Icon
-          name='history'
-          type='font-awesome'
-          size={24}
-          color={ navigation.isFocused ? '#006025' : '#777f7c' }
-        />
-      ),
-    })
   },
   Favorites: {
     screen: Favorites,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'My Favorites',
-      drawerIcon: () => (
-        <Icon
-          name='like1'
-          type='antdesign'
-          size={24}
-          color={ navigation.isFocused ? '#006025' : '#777f7c' }
-        />
-      ),
-    })
   },
   Contact: {
     screen: Contact,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'Contact',
-      drawerIcon: () => (
-        <Icon
-          name={'contacts'}
-          size={24}
-          type={'ant-design'}
-          color={ navigation.isFocused ? '#006025' : '#777f7c' }
-        />
-      ),
-    })
   },
   About: {
     screen: About,
-    navigationOptions: ({ navigation }) => ({
-      drawerLabel: 'About Triple 7',
-      drawerIcon: () => (
-        <Icon
-          name='info-outline'
-          type='material-icons'
-          size={24}
-          color={ navigation.isFocused ? '#006025' : '#777f7c' }
-        />
-      ),
-    })
   }
 },{
   initialRouteName: 'Menu',
   contentComponent: CustomDrawerContentComponent,
   contentOptions: {
-    activeTintColor: '#006025',
-    activeBackgroundColor: '#bde8cd',
+    activeTintColor: 'black',
+    activeBackgroundColor: '#e6e6e6',
   }
 });
 
