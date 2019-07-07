660c1b18be52ec52b03e5550d9d01dcc2af9acfa
Ike Njoku <ikeenjoku@gmail.com>
2019-07-07 09:41:16 +0100

    fix darwer theme

--- src/components/CustomDrawerContentComponent.js
+++ src/components/CustomDrawerContentComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import { DrawerItems, SafeAreaView } from 'react-navigation';
 import { Avatar, Divider, Icon } from 'react-native-elements';
@@ -8,18 +9,23 @@ import ColorPalette from '../components/ColorPalette.js';
 class CustomDrawerContentComponent extends Component {
 
   render() {
+    const { theme } = this.props;
     return (
       <ScrollView>
-        <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
-          <View style={{ paddingTop: '4%', paddingBottom: '4%', backgroundColor:'#009145' }}>
-            <View style={{ justifyContent: 'center', alignItems:'center' }}>
-              <Avatar size='large' rounded icon={{ name: 'user-circle-o', type:'font-awesome', size: 80 }} />
+        <SafeAreaView
+          style={styles.container}
+          forceInset={{ top: 'always', horizontal: 'never' }}
+        >
+          <View style={[ styles.containHeader, { backgroundColor: theme.pri700 }]}>
+            <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+              <Avatar size='large' rounded icon={{ name: 'user-circle-o', type: 'font-awesome', size: 80 }} />
               <Text style={{ color: '#f9f9f9', marginTop: '3%', fontFamily: 'sans-serif-condensed' }}>Hi Jenie</Text>
               <Text style={{ color: '#f9f9f9', fontFamily: 'sans-serif-condensed' }}>jenie@gmail.com</Text>
             </View>
-
           </View>
+
           <DrawerItems {...this.props} />
+
           <View>
             <View style={{ marginTop: '2%' }}>
               <Divider style={{ backgroundColor: '#777f7c90' }} />
@@ -30,23 +36,23 @@ class CustomDrawerContentComponent extends Component {
             <View style={{ marginTop: '5%' }}>
               <Divider style={{ backgroundColor: '#777f7c90' }} />
             </View>
-            <View style={{ backgroundColor: '#d1d1d1' }}>
-              <View style={{paddingLeft: '6%', flexDirection:'row', alignItems: 'center', paddingBottom: '2%', paddingTop: '2%'}}>
+            <View style={{ backgroundColor: '#e6e6e6' }}>
+              <View style={[styles.containDrawerOption, { paddingBottom: '2%' }]}>
                 <Icon
                   name='logout'
                   type='simple-line-icon'
                   size={20}
-                  color={'#00602580'}
+                  color={`${theme.pri900}80`}
                   containerStyle={{ marginRight: '10%' }}
                 />
                 <Text style={{ color: 'black', fontFamily: 'sans-serif-medium' }}>Log Out</Text>
               </View>
-              <View style={{paddingLeft: '6%', flexDirection:'row', alignItems: 'center', paddingBottom: '6%', paddingTop: '2%'}}>
+              <View style={[styles.containDrawerOption, { paddingBottom: '6%' }]}>
                 <Icon
                   name='user-secret'
                   type='font-awesome'
                   size={24}
-                  color={'#00602590'}
+                  color={`${theme.pri900}90`}
                   containerStyle={{ marginRight: '10%' }}
                 />
                 <Text style={{ color: 'black', fontFamily: 'sans-serif-medium' }}>Developer</Text>
@@ -63,6 +69,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  containHeader: {
+    paddingTop: '4%',
+    paddingBottom: '4%'
+  },
+  containDrawerOption: {
+    paddingLeft: '6%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: '2%',
+  }
 });
 
-export default CustomDrawerContentComponent;
+const mapStateToProps = ({ themeReducer }) => ({
+  theme: themeReducer.theme,
+});
+
+export default connect(mapStateToProps, {})(CustomDrawerContentComponent);
