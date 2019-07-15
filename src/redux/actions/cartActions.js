ee00b8c45f2b685b8d788744616768bb0c58ca1e
Ike Njoku <ikeenjoku@gmail.com>
2019-07-15 11:33:30 +0100

    update cart actions

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -3,7 +3,13 @@ import {
   REMOVE_FROM_CART,
   CLEAR_CART,
   HOME_DELIVERY,
+  MAKE_ORDER,
+  MAKE_ORDER_SUCCESS,
+  MAKE_ORDER_FAILURE,
 } from '../actionTypes';
+import API from '../axiosConfig';
+import { toastError } from './notifications';
+import NavigationService from '../../navigation/NavigationService';
 
 export const addToCart = (prevCart, {name, price}) => {
   let newCart;
@@ -51,3 +57,34 @@ export const toggleDelivery = (homeDelivery) => ({
   type: HOME_DELIVERY,
   payload: !homeDelivery,
 });
+
+export const make_order = () => ({
+  type: MAKE_ORDER,
+});
+
+export const make_order_success = () => ({
+  type: MAKE_ORDER_SUCCESS,
+});
+
+export const make_order_failure = (error) => ({
+  type: MAKE_ORDER_FAILURE,
+  error,
+});
+
+export const makeAnOrder = (order) => (dispatch) => {
+  dispatch(make_order());
+  return  API.post('/orders', order)
+    .then(() => {
+      dispatch(make_order_success());
+      NavigationService.navigate('Payment');
+    })
+    .catch(error => {
+      if (error.response) {
+        dispatch(make_order_failure(error.response.data));
+        toastError(error.response.data.message);
+      } else {
+        toastError('Network Error! Check your internet connection and retry');
+        dispatch(make_order_failure({ message: 'Error registering user' }));
+      }
+    });
+};
